fe00c1d6720f96789f0a45e91fb09025d3c197d9
denaatorabi@gmail.com <denaatorabi@gmail.com>
2024-08-20 23:29:38 +0330

    u2

---
+++ HotelReservationProject-Laravel/database/migrations/2024_04_27_141734_create_rooms_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('rooms', function (Blueprint $table) {
+            $table->id();
+            $table->integer('number')->unique();
+            $table->integer('capacity');
+            $table->string('rate');
+            $table->text('facilities'); // Storing facilities as a text field
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('rooms');
+    }
+};
